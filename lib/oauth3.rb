abe55c69be9d5dc44983ad3c2cf6e0dd6aab35f4
AJ ONeal <awesome@coolaj86.com>
2015-04-14 20:11:54 -0600

    update for oauth3.html

--- lib/oauth3.rb
+++ lib/oauth3.rb
@@ -60,19 +60,89 @@ class Oauth3
     (0...50).map { ('a'..'z').to_a[rand(26)] }.join
   end
 
-  def authorize_url(provider_uri)
+  def authorize_url(provider_uri, params)
+    provider_uri = @@oauth3.normalize_provider_uri(provider_uri)
     redirect_uri = @options[:redirect_uri]
     rnd = random_string()
-    @states[rnd] = Time.now
+
+    @states[rnd] = {
+      created_at: Time.now,
+      provider_uri: provider_uri,
+      params: params
+    }
 
     # TODO state should go in params to the provider, not the redirect directly
     # ... but ultimately it has the same effect, so whatever
-    get_oauth2_client(provider_uri).auth_code.authorize_url(
+    get_oauth2_client(provider_uri).auth_code.authorize_url({
       # TODO (change ? to & if there's already a ?)
-      redirect_uri: redirect_uri +
-        "?provider_uri=" + URI.encode_www_form_component(provider_uri) +
-        "&state=" + rnd
-    )
+      redirect_uri: redirect_uri + "?state=" + rnd
+    })
+  end
+
+  def authorization_code_callback(params)
+    # TODO needs error handling function to make this DRY
+
+    if not params[:state]
+      if params[:error]
+        return params
+      else
+        return { error: "E_NO_STATE", error_description: "provider_did_not_return_state" }
+      end
+    end
+
+    original_state = @@oauth3.get_state(params[:state])
+    if not original_state
+      return { error: "E_INVALID_SERVER_STATE", error_description: "server_state_missing_or_expired" }
+    end
+
+    # TODO provider_uri should not be necessary because we have state
+    # (but I don't think oauth3.html implements that completely yet)
+    # original_state = { created_at, provider_uri, params }
+    browser_params = original_state[:params].merge({ provider_uri: original_state[:provider_uri] })
+    if not browser_params
+      browser_params = { error: "E_SANITY_FAIL", error_description: "sanity_fail_server_state_missing_params" }
+      params.delete(:state)
+      return params.merge(browser_params)
+    end
+
+    browser_state = browser_params[:state] or browser_params[:browser_state]
+    if not browser_state
+      browser_params[:error] = "E_INVALID_BROWSER_STATE"
+      browser_params[:error_description] = "state_missing_or_expired"
+      return browser_params
+    end
+
+    # TODO decide on one or the other. Favor explicit to avoid confusion?
+    result_params = { browser_state: browser_state, state: browser_state }
+    if params[:error]
+      params.delete(:state)
+      return params.merge(result_params)
+    end
+
+    code = params[:code]
+
+    if not code
+      result_params[:error] = "E_INVALID_BROWSER_STATE"
+      result_params[:error_description] = "state_missing_or_expired"
+      params.delete(:state)
+      return params.merge(result_params)
+    end
+
+    provider_uri = browser_params[:provider_uri]
+    if token = @@oauth3.get_token(provider_uri, code).token
+      result_params[:access_token] = token
+    end
+
+    # Note in the future the server will send back
+    #   granted_scopes=foo,bar,baz,etc
+    #   expires_at=2015-04-01T12:30:00.000Z
+    #   app_scoped_id=<<default-account-app-scoped-id>>
+    params.delete(:state)
+    params.merge(result_params)
+  end
+
+  def get_state(state)
+    @states.delete(state)
   end
 
   def validate_state(provider_uri, state)
